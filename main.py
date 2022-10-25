da1b17466fc5078d90f1b7b1da13006b97965f59
Adrian34557 <114925401+Adrian34557@users.noreply.github.com>
2022-10-25 20:07:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,62 @@
+# print("wszystkie dni listopada")
+# for i in range(1,31):
+#       print(i)
+#Zad.2
+# a = int(input("Podaj Zakres"))
+# for i in range(1,a+1):
+#     if i % 2!=0:
+#         print(i**2)
+        
+# #Zad.3
+# for i in range(1000,10000):
+#     if i%379 == 0:
+#         print(i)
+        
+# #Zad.4
+# for i in range(100,1000):
+#     if i%5==0 and i%6==0 and i%11==0:
+#         print(i)      
+# #Zad.5
+# n = int(input("Ile liczb:"))
+# suma = 0
+# for i in range(n):
+#     a = int(input("Podaj liczby:"))
+#     suma = suma+a
+#     print(suma)
+    
+# # Zad.6
+# k=int(input("podaj liczbe k:"))
+# suma=0
+# for i in range (k):
+#     suma = k * 2
+#     print("suma:",suma)
+
+# zad 7
+#m = int(input("Ilosc liczb: "))
+#suma = 0
+#for i in range(11, (m*2)+11, 2):
+#    if i<100:
+#      suma += i
+#print("Suma:", suma)
+
+# zad 8
+#w0 = int(input("Podaj kwote wejsciowa:"))
+#l = int(input("Ilosc lat:"))
+#kap = 0
+#wartosc = w0
+#for i in range(1, l+1):
+#    kap = wartosc * 0.06
+#    wartosc = wartosc + kap
+#print("Wartosc:", round(wartosc, 2))
+
+# zad 9
+#n = int(input("Ilosc liczb: "))
+#temp = 0
+#suma = 0
+#for i in range(n):
+#    temp = i*100+21
+#    suma += temp
+#print("Suma:", suma)
+
+#10
+#________
